113b4a23e19c684b124478bc59878e3f1590db24
heitorhidalgo <heitorhidalgo10@hotmail.com>
2025-12-24 16:52:02 -0300

    feat: i innicio da implementação d da pagina

---
+++ lib/busca_cep_page/busca_page.dart
@@ -0,0 +1,15 @@
+import 'package:aplicativo_viacep/busca_cep_page/busca_component.dart';
+import 'package:flutter/material.dart';
+
+class BuscaPage extends StatefulWidget with BuscaPageComponent {
+  @override
+  State<BuscaPage> createState() => _BuscaPageState();
+}
+
+class _BuscaPageState extends State<BuscaPage> with BuscaPageComponent {
+  @override
+  build(BuildContext context) {
+    initialize(context);
+    return Scaffold(body: body());
+  }
+}
