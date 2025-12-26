20ac67a5115594eedc993610141f21efeca809e3
heitorhidalgo <heitorhidalgo10@hotmail.com>
2025-12-26 13:43:44 -0300

    refactor: atualizacao no controller

--- lib/busca_page/busca_controller.dart
+++ lib/busca_page/busca_controller.dart
@@ -3,4 +3,13 @@ import 'package:get/get.dart';
 
 class BuscaPageController extends GetxController {
   late BuildContext context;
+
+  //TextEditingController para chamar o teclado
+  final TextEditingController digitaCep = TextEditingController();
+
+  //funcao para busca
+  void buscarCep() {
+    String cepDigitado = digitaCep.text;
+    print("teste: $cepDigitado");
+  }
 }
